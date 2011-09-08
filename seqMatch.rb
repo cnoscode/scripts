982913f17bfe5533941c26d134deccb81f65fcb6
Cyril Jose <cyril_jose@ymail.com>
2011-09-08 16:27:40 -0400

    started loop - still bugs to fix

--- seqMatch.rb
+++ seqMatch.rb
@@ -13,17 +13,17 @@ f5_cs.pos = 0
 
 
 while f3_entry = f3_cs.next_entry && f5_entry = f5_cs.next_entry
-	if f3_entry =~ /^>/
-		start_of_entry = f3_cs.pos
-		if f3_entry =~ /F3/
-			start_of_f3 = f3_cs.pos
-			length = start_of_f3 - start_of_entry
-			header = f3_entry.read(length)
-			f3_out.puts header
-		end
-	end
+  if f3_entry =~ /^>/
+    start_of_entry = f3_cs.pos
+    if f3_entry =~ /F3/
+      start_of_f3 = f3_cs.pos
+      length = start_of_f3 - start_of_entry
+      header = f3_entry.read(length)
+      f3_out.puts header
+    end
+  end
 end
-	
+  
 # F3, F5-P2
 f3_cs.close
 f5_cs.close
