17c2004c4aa32b259758b0a036b31f1fe1abe6c0
Cyril Jose <cyril_jose@ymail.com>
2011-09-12 16:40:34 -0400

    more efficient way of reading without using arrays - more work needed

--- seqMatch.rb
+++ seqMatch.rb
@@ -1,35 +1,31 @@
 require 'rubygems'
-require 'bio'
 
-# find csfasta/qual files 
-cs_files = Dir.glob("/Users/cjose/Desktop/testfiles/*.csfasta")
-qual_files = Dir.glob("/Users/cjose/Desktop/testfiles/*.qual")
+cs_files = Dir.glob("/Users/cjose/Desktop/scripts/*.csfasta")
+qual_files = Dir.glob("/Users/cjose/Desktop/scripts/*.qual")
 
-# output files
+f3_file = File.open(cs_files[0])
+f5_file = File.open(cs_files[1])
+
 f3_out = File.new("/Users/cjose/Desktop/f3.csfasta", "w")
 f5_out = File.new("/Users/cjose/Desktop/f5.csfasta", "w")
 
-f3_file = Bio::FastaFormat.open(cs_files[0])
-f5_file = Bio::FastaFormat.open(cs_files[1])
-
-# header pattern to match
-pattern = /[0-9]*[_][0-9]*[_][0-9]*[_]/
-f3_headers = []
-f5_headers = []
-
-while f3_entry = f3_file.next_entry #&& f5_entry = f5_file.next_entry
-  if f3_entry.definition =~ pattern   
-    f3_headers.push(f3_entry.definition.gsub(/F3/,""))
+while !f3_file.eof?
+  while !f5_file.eof?
+    f5_ln = f5_file.readline
+    if f5_ln =~ /(\d*_\d*_\d*_)/
+      f5_ln += f5_file.readline
+    end
   end
-  while f5_entry = f5_file.next_entry
-    if f5_entry.definition =~ pattern
-      f5_headers.push(f5_entry.definition.gsub(/F5/,""))
-    end
-    f3_headers.each do |f3_line|
-      f5_headers.each do |f5_line|
-        f3_out.puts f3_entry if f3_line == f5_line
-        f5_out.puts f5_entry if f5_line == f3_line
-      end
-    end
+  f3_ln = f3_file.readline
+  if f3_ln =~ /(\d*_\d*_\d*_)/
+    f3_ln += f3_file.readline
+  end
+  
+end
+
+if f3_ln.delete("F3").size == f5_ln.delete("F5-P2").size 
+  if true
+    f3_out.puts f3_ln
+    f5_out.puts f5_ln  
   end
 end
