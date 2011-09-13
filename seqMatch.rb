e723ecd16ef7a54f14f763e6ccc4650f06974e7c
Cyril Jose <cyril_jose@ymail.com>
2011-09-13 16:53:01 -0400

    figured out how to compare headers in both files

--- seqMatch.rb
+++ seqMatch.rb
@@ -9,23 +9,25 @@ f5_file = File.open(cs_files[1])
 f3_out = File.new("/Users/cjose/Desktop/f3.csfasta", "w")
 f5_out = File.new("/Users/cjose/Desktop/f5.csfasta", "w")
 
-while !f3_file.eof?
-  while !f5_file.eof?
-    f5_ln = f5_file.readline
-    if f5_ln =~ /(\d*_\d*_\d*_)/
-      f5_ln += f5_file.readline
-    end
-  end
-  f3_ln = f3_file.readline
-  if f3_ln =~ /(\d*_\d*_\d*_)/
-    f3_ln += f3_file.readline
-  end
-  
-end
+while !f3_file.eof? && while !f5_file.eof?
+		f3_ln = f3_file.readline.chomp
+		f5_ln = f5_file.readline.chomp
+		if f3_ln =~ /^>\d*_\d*_\d*_F3$/
+			f3_headers = f3_ln.gsub(/F3/, "")		
+		end
+		if f5_ln =~ /^>\d*_\d*_\d*_F5-P2$/
+			f5_headers = f5_ln.gsub(/F5-P2/, "")
+			if f3_headers == f5_headers
+				puts f3_headers
+				puts f5_headers
+				puts "worked"
+			else
+				puts f3_headers
+				puts f5_headers
+				puts "nope"
+			end
+		end
 
-if f3_ln.delete("F3").size == f5_ln.delete("F5-P2").size 
-  if true
-    f3_out.puts f3_ln
-    f5_out.puts f5_ln  
-  end
-end
+	end # end while	
+end # end while
+
