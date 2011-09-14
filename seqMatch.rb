b905ce8f0324262e0fca7c5c85793424f7a1f1e2
Cyril Jose <cyril_jose@ymail.com>
2011-09-14 16:39:39 -0400

    added buffer but still needs adjustments

--- seqMatch.rb
+++ seqMatch.rb
@@ -18,31 +18,26 @@ while !f5_file.eof? && !f3_file.eof?
   f3_id = $1
   f5_ln =~ /^>(\d*_\d*_\d*_)/
   f5_id = $1
+ 
   if f3_id == f5_id 
     f3_out.puts f3_ln
-    f3_out.puts f3_file.readline
-    f5_file.readline
-
-#    if buffer[0] =~ /#{buffer[1]}/
-#      f3_out.puts f3_ln
-#      f3_out.puts f3_file.readline
-#    end
+    f3_out.puts f3_file.readline # start of next f3 header
+    f5_file.readline # start of next f5 header
   else # >1_10_140_F3 and >1_9_1912_F5-P2
-    puts f3_file.readline
-    puts f5_file.readline # end of seq for >1_9_1912_F5-P2; start of next header >1_10_140_F5-P2
-    # create buffer
-    buffer = ["",""]     
-    puts buffer[0] = f5_ln # 1_10_140_
+    #puts f3_file.readline # outputs last seq line then eof
+    f5_file.readline # end of seq for >1_9_1912_F5-P2; start of next header >1_9_8888_F5-P2
+    
+    ctr = 0
+    buffer = []    
+    puts buffer = f5_id # >1_9_8888_F5-P2
+
+    if buffer =~ /#{f3_id}/
+      f3_out.puts f3_ln
+      f3_out.puts f3_file.readline
+    else
+      f5_file.readline  
+    end    
   end
-     
-#  if f3_file.eof? or f5_file.eof?
-#    #puts f3_ln, f5_ln
-#    if f3_ln =~ /#{f3_ln}/
-#      #puts "works"
-#      #puts f5_file.pos
-#    end
-#    # puts f3_file.pos
-#  end
 end
 f3_file.close
 f5_file.close
