31fb020292d78d18da86e9fda29ec613ac5fe2d6
Cyril Jose <cyril_jose@ymail.com>
2011-09-20 16:45:39 -0400

    buffer needs work

--- seqMatch.rb
+++ seqMatch.rb
@@ -20,15 +20,15 @@ while !f5_file.eof? && !f3_file.eof?
   if f3_id == f5_id 
     f3_out.puts f3_ln
     f3_out.puts f3_file.readline 
-    f5_  file.readline 
+    f5_file.readline 
   else 
-    f3_file.readline 
-    f5_file.readline   
-     ctr = 0
-    max_buff = 7 # 8 total (current F5 line)
-    # start of buffer storage
+    f5_file.readline
+    f5_pos = f5_file.pos  
+    puts f5_pos
     buffer = []
-    buffer.push f5_ln   
+    ctr = 0
+    max_buff = 7 # entries in buffer
+    buffer.push f5_ln # push current F5 line      
     while ctr < max_buff  
     ln = f5_file.readline.chomp
       if ln =~ /^>/
@@ -36,14 +36,23 @@ while !f5_file.eof? && !f3_file.eof?
         ctr += 1
       end #end if
     end # end while
-    p buffer
+    
     buffer.each do |header|
-     if header =~ /#{f3_id}/
-       f3_out.puts f3_ln
-       f3_out.puts f3_file.readline 
-     end 
-     end # end do iterator
-  end #end top level if 
-end # while
+      if header =~ /#{f3_id}/
+         f3_out.puts f3_ln
+         f3_out.puts f3_file.readline
+       else
+         #f3_file.readline # next entry
+       end
+    end # end do iterator
+    buffer.clear
+    #p f5_pos
+    f5_file.rewind
+    h = f5_file.read(f5_pos) 
+    puts h
+  end #end top level if
+
+end # end while
+
 f3_file.close
 f5_file.close
