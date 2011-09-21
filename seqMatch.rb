74ea11b013ce682d3cbdc2804c933398b4caecb5
Cyril Jose <cyril_jose@ymail.com>
2011-09-21 16:25:11 -0400

    F5 as template works, now fixing bugs for F3 prime

--- seqMatch.rb
+++ seqMatch.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 
 cs_files = Dir.glob("/Users/cjose/Desktop/scripts/*.csfasta")
-qual_files = Dir.glob("/Users/cjose/Desktop/scripts/*.qual")
 
 f3_file = File.open(cs_files[0])
 f5_file = File.open(cs_files[1])
@@ -16,43 +15,70 @@ while !f5_file.eof? && !f3_file.eof?
   f3_id = $1
   f5_ln =~ /^>(\d*_\d*_\d*_)/
   f5_id = $1
- 
+
   if f3_id == f5_id 
     f3_out.puts f3_ln
     f3_out.puts f3_file.readline 
     f5_file.readline 
-  else 
+  else
     f5_file.readline
-    f5_pos = f5_file.pos  
-    puts f5_pos
-    buffer = []
-    ctr = 0
-    max_buff = 7 # entries in buffer
-    buffer.push f5_ln # push current F5 line      
-    while ctr < max_buff  
+    ctr = 0          
+    while ctr < 7 # check next 7 entries 
     ln = f5_file.readline.chomp
       if ln =~ /^>/
-        buffer.push ln
-        ctr += 1
-      end #end if
-    end # end while
-    
-    buffer.each do |header|
-      if header =~ /#{f3_id}/
-         f3_out.puts f3_ln
-         f3_out.puts f3_file.readline
-       else
-         #f3_file.readline # next entry
+        if ln =~ /#{f3_id}/
+          f3_out.puts f3_ln
+          f3_out.puts f3_file.readline
+          f5_file.readline
+          break
+        else
+          ctr += 1
+        end #end if
        end
-    end # end do iterator
-    buffer.clear
-    #p f5_pos
-    f5_file.rewind
-    h = f5_file.read(f5_pos) 
-    puts h
-  end #end top level if
+     end 
 
-end # end while
+  end #end top level if
+end 
 
 f3_file.close
+f3_out.close
 f5_file.close
+
+f5_file = File.open(cs_files[1])
+f3_prime = File.open("/Users/cjose/Desktop/f3.csfasta", "r")
+f5_file.rewind
+
+while !f5_file.eof? && !f3_prime.eof?
+  f3pr_ln = f3_prime.readline.chomp
+  f5_ln = f5_file.readline.chomp
+  f5_ln =~ /^>(\d*_\d*_\d*_)/
+  f5_id = $2
+  f3pr_ln =~ /^>(\d*_\d*_\d*_)/
+  f3pr_id = $2
+   
+  if f5_id == f3pr_id
+    f5_out.puts f5_ln
+    f5_out.puts f5_file.readline 
+    f3_prime.readline 
+  else
+    f3_prime.readline
+    ctr = 0          
+    while ctr < 7 # 7 entries in buffer
+    ln = f3_prime.readline.chomp
+      if ln =~ /^>/
+        if ln =~ /#{f5_id}/
+          f5_out.puts f5_ln
+          f5_out.puts f5_file.readline
+          f3_prime.readline
+          break
+        else 
+          ctr += 1
+        end #end if
+      end
+    end 
+
+  end #end top level if
+end  
+
+
+
