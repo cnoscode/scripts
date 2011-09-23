12dd0678e6d98e5732b21a242e96271615573706
Cyril Jose <cyril_jose@ymail.com>
2011-09-23 11:48:34 -0400

    testing out bigger files to see if actually works

--- seqMatch.rb
+++ seqMatch.rb
@@ -1,84 +1,89 @@
 require 'rubygems'
 
-cs_files = Dir.glob("/Users/cjose/Desktop/scripts/*.csfasta")
+f3_cs_qual = Dir.glob("/Users/cjose/Desktop/subset/*csfasta")
+#f5_cs_qual = Dir.glob("/Users/cjose/Desktop/files/*F5-P2*")
 
-f3_file = File.open(cs_files[0])
-f5_file = File.open(cs_files[1])
+f3_cs_file = File.open(f3_cs_qual[0]) # csfasta
+f3_qual_file = File.open(f3_cs_qual[1]) # qual
 
-f3_out = File.new("/Users/cjose/Desktop/f3.csfasta", "w")
-f5_out = File.new("/Users/cjose/Desktop/f5.csfasta", "w")
+f3_cs_out = File.new("/Users/cjose/Desktop/f3_out.csfasta", "w")
+f3_qual_out = File.new("/Users/cjose/Desktop/f3_out.qual", "w")
 
-while !f5_file.eof? && !f3_file.eof?
-  f3_ln = f3_file.readline.chomp
-  f5_ln = f5_file.readline.chomp
+while !f3_qual_file.eof? && !f3_cs_file.eof?
+  f3_ln = f3_cs_file.readline.chomp
+  f3_qual_ln = f3_qual_file.readline.chomp
   f3_ln =~ /^>(\d*_\d*_\d*_)/
   f3_id = $1
-  f5_ln =~ /^>(\d*_\d*_\d*_)/
-  f5_id = $1
+  f3_qual_ln =~ /^>(\d*_\d*_\d*_)/
+  f3_qual_id = $1
 
-  if f3_id == f5_id 
-    f3_out.puts f3_ln
-    f3_out.puts f3_file.readline 
-    f5_file.readline 
+  if f3_id == f3_qual_id 
+    f3_cs_out.puts f3_ln
+    f3_cs_out.puts f3_cs_file.readline 
+    f3_qual_file.readline 
   else
-    f5_file.readline
+    f3_qual_file.readline
     ctr = 0
-    break_pos = f5_file.pos          
-    while ctr < 7 # check next 7 entries 
-    ln = f5_file.readline.chomp
-      if ln =~ /^>/
-        if ln =~ /#{f3_id}/
-          f3_out.puts f3_ln
-          f3_out.puts f3_file.readline
-          f5_file.readline
+    break_pos = f3_qual_file.pos          
+    while ctr < 10 # check next 7 entries 
+    f3_ln = f3_qual_file.readline.chomp
+      if f3_ln =~ /^>/
+        if f3_ln =~ /#{f3_id}/
+          f3_cs_out.puts f3_ln
+          f3_cs_out.puts f3_cs_file.readline
+          f3_qual_file.readline
           break
         elsif ctr == 6
-          f3_file.readline
+          f3_cs_file.readline
           ctr += 1
         end #end if
        end
     end 
-		f5_file.pos = break_pos
+		f3_qual_file.pos = break_pos
   end #end top level if
 end 
 
-f3_file.close
-f3_out.close
-f5_file.close
+f3_cs_file.close
+f3_cs_out.close
+f3_qual_file.close
 
-f5_file = File.open(cs_files[1])
-f3_prime = File.open("/Users/cjose/Desktop/f3.csfasta", "r")
+f3_qual_file = File.open(f3_cs_qual[1])
+f3_prime = File.open("/Users/cjose/Desktop/f3_out.csfasta", "r")
 
-while !f5_file.eof? && !f3_prime.eof?
+while !f3_qual_file.eof? && !f3_prime.eof?
+  f3_pos = f3_prime.pos
   f3pr_ln = f3_prime.readline.chomp
-  f5_ln = f5_file.readline.chomp
-  f5_ln =~ /^>(\d*_\d*_\d*_)/
-  f5_id = $1
+  f3_qual_ln = f3_qual_file.readline.chomp
+  f3_qual_ln =~ /^>(\d*_\d*_\d*_)/
+  f3_qual_id = $1
   f3pr_ln =~ /^>(\d*_\d*_\d*_)/
   f3pr_id = $1
-   
-  if f5_id == f3pr_id
-    f5_out.puts f5_ln
-    f5_out.puts f5_file.readline
+
+  if f3_qual_id == f3pr_id
+    f3_qual_out.puts f3_qual_ln
+    f3_qual_out.puts f3_qual_file.readline
     f3_prime.readline 
   else
+    f3_prime.pos = f3_pos
     f3_prime.readline
     ctr = 0          
-    break_pos = f3_prime.pos
-    while ctr < 2 # 7 entries in buffer
-    ln = f3_prime.readline.chomp
-   	if ln =~ /^>/
-    	if ln =~ /#{f5_id}/
-      	f5_out.puts f5_ln
-      	f5_out.puts f5_file.readline
+    f5_pos = f3_qual_file.pos
+    while ctr < 10
+    f3_qual_ln = f3_qual_file.readline.chomp
+   	if f3_qual_ln =~ /^>/
+    	if f3_qual_ln =~ /#{f3pr_id}/
+      	f3_qual_out.puts f3_qual_ln
+      	f3_qual_out.puts f3_qual_file.readline
       	f3_prime.readline
-      	break
-      elsif ctr == 1
-      	f5_file.readline
+        f5_pos = f3_qual_file.pos
+        f3_pos = f3_prime.pos
+        break
+      else
+      	f3_qual_file.readline
         ctr += 1 
       end # end if
-     end
-    end 
-		f3_prime.pos = break_pos
+   end
+  end 
+		#f3_prime.pos = break_pos
   end # end top level if
 end 
